9bd75b76efff365569389f4e3e982931266a4bdb
Maria-Jara <mariaferjarami@gmail.com>
2024-12-04 19:21:22 -0500

    Ejercicio 22

---
+++ Ejercicio 18/Ejercicio_22_poo/S2_Ejercicio_11_Ejercicio_propuesto_22.py
@@ -0,0 +1,11 @@
+#S2_Ejercicio_11_Ejercicio_propuesto_22
+
+a = float(input("Ingrese el valor de A: "))
+b = float(input("Ingrese el valor de B: "))
+
+if a > b:
+    print("A es mayor que B")
+elif a == b:
+    print("A es igual que B")
+else:
+    print("A es menor que B")
